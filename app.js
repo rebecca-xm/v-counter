85c6b6fc34caf7c25701981ada07d742b2e7c49f
Rebecca Mollica <rebecca.mollica@live.it>
2022-04-22 10:45:53 +0200

    feat: add input value

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      text: ''
     };
   },
   methods: {
@@ -18,6 +19,12 @@ const app = Vue.createApp({
     reduceFive(num) {
       // this.counter = this.counter - 5
       this.counter = this.counter - num
+    },
+    outputText(e) {
+      this.text = e.target.value
+      // qualora fosse necessario passare un secondo parametro alla funzione
+      // e (event) andrebbe richiamato con la parola chiave $event nella parte HTML
+      // es => v-on:input="outputText($event, 'altro argomento')"
     }
   }
 });
